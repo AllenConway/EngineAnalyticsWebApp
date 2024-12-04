cf7cb41e701907795ae9f2ad0cc06b9753af313e
Allen Conway <allen.conway@xebia.com>
2024-12-04 18:18:25 -0500

    Initial commit of the EngineAnalyticsWebApp solution

---
+++ EngineAnalyticsWebApp.Shared/Models/Engine/Automobile.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EngineAnalyticsWebApp.Shared.Models.Engine
+{
+    public class Automobile
+    {
+        [Required]
+        public int? Year { get; set; }
+        [Required]
+        public string? Make { get; set; }
+        [Required]
+        public string? Model { get; set; }
+        [Required]
+        [ValidateComplexType]
+        public Horsepower? Horsepower { get; set; }
+        public Displacement? Displacement { get; set; }
+        public Torque? Torque { get; set; }
+        [ValidateComplexType]
+        public EngineAnalytics? EngineAnalytics { get; set; }
+    }
+}
