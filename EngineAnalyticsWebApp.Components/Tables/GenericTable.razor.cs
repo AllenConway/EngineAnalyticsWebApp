756cf5ca6fa4d669a0aba3146cecdfac6db5fc18
Allen Conway <allen.conway@xebia.com>
2025-09-08 22:50:07 -0400

    Added generic and dymanic component examples

---
+++ EngineAnalyticsWebApp.Components/Tables/GenericTable.razor.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Microsoft.AspNetCore.Components;
+
+namespace EngineAnalyticsWebApp.Components.Tables
+{
+    public partial class GenericTable<TItem>
+    {
+        [Parameter] public IEnumerable<TItem> Items { get; set; } = [];
+        [Parameter] public RenderFragment? HeaderCells { get; set; }
+        [Parameter] public RenderFragment<TItem>? RowCells { get; set; }
+    }
+}
---
+++ EngineAnalyticsWebApp.UI/Components/Pages/Reports/DynamicReports.razor.cs
@@ -0,0 +1,23 @@
+﻿using EngineAnalyticsWebApp.Components.Reporting;
+using Microsoft.AspNetCore.Components;
+
+namespace EngineAnalyticsWebApp.UI.Components.Pages.Reports
+{
+    public partial class DynamicReports
+    {
+        private Type? selectedReportType = null;
+        private Dictionary<string, object> reportParameters = new();
+
+        private void OnReportChanged(ChangeEventArgs e)
+        {
+            var value = e.Value?.ToString();
+            selectedReportType = value switch
+            {
+                "Displacement" => typeof(DisplacementDataGrid),
+                "Torque"       => typeof(TorqueDataGrid),
+                _              => null
+            };
+            reportParameters.Clear();
+        }
+    }
+}
